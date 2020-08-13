8c0f4de658d5478b353ae3d5d3d94bd41b314fb3
Jan Schneider <jan.schneider@qsc.de>
2020-08-13 11:14:59 +0200

    minor backend changes

--- back/main.py
+++ back/main.py
@@ -1,10 +1,28 @@
+'''
+Baufuchs Online-Shop backend
+Python 3.x
+Modules: Flask, 
+'''
+
+## importing modules
 from flask import Flask
+
+## setting session vars
 app = Flask(__name__)
 
-
+## routes
+# for quick return tests
 @app.route('/api/hello')
-def hello_world():
+def api_hello():
     return 'Hello, World!'
+
+# for getting shop item details to the web-client
+@app.route('/api/item/details')
+@app.route('/api/item/list')
+def api_item():
+    data = {}
+    return data
+
 
 
 
