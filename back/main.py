d16cd81a541bf030d4c80e0b9bdbdeb04779bb1e
Jan Schneider <jan.schneider@qsc.de>
2020-08-14 10:29:53 +0200

    changed test route to return json

--- back/main.py
+++ back/main.py
@@ -5,25 +5,54 @@ Modules: Flask,
 '''
 
 ## importing modules
-from flask import Flask
+from flask import Flask, request, jsonify
+import mysql.connector
+
+
 
 ## setting session vars
 app = Flask(__name__)
 
+'''
+sql_con = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword"
+)
+sql_cur = sql_con.cursor()
+'''
+
+
+
+
+
 ## routes
 # for quick return tests
-@app.route('/api/hello')
+@app.route('/api/hello', methods=['GET'])
 def api_hello():
-    return 'Hello, World!'
+    data = {
+        "username": "Test",
+        "password": "Testkey"
+    }
+    resp = jsonify(username="Test",
+        password="Testkey")
+    resp.status_code = 200
+    return resp
 
 # for getting shop item details to the web-client
-@app.route('/api/item/details')
-@app.route('/api/item/list')
+@app.route('/api/item/details', methods=['GET', 'POST'])
+def api_item_details():
+    data = sql_get()
+    return data
+@app.route('/api/item/list', methods=['GET', 'POST'])
 def api_item():
-    data = {}
+    data = sql_get()
     return data
 
-
+# for sending and applying score reviews from users
+@app.route('/api/item/score', methods=['GET', 'POST'])
+def api_item_score():
+    data = request
 
 
 
