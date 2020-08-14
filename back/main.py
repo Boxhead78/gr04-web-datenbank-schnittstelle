8b0961f96f08abb10dae57e80795a790c408e064
Jonas <Jojoleupolt@googlemail.com>
2020-08-14 13:08:57 +0200

    modified api_item_details function with sql query in main.py

--- back/main.py
+++ back/main.py
@@ -1,33 +1,36 @@
 '''
 Baufuchs Online-Shop backend
 Python 3.x
-Modules: Flask, 
+Modules: Flask 
 '''
 
-## importing modules
+# importing modules
 from flask import Flask, request, jsonify
 import mysql.connector
 
 
-
-## setting session vars
+# setting session vars
 app = Flask(__name__)
 
-'''
+
 sql_con = mysql.connector.connect(
     host="localhost",
-    user="yourusername",
-    password="yourpassword"
+    user="root",
+    password="",
+    database="baufuchs"
 )
-sql_cur = sql_con.cursor()
+sql_cur = sql_con.cursor(buffered=True)
+'''
+sql_cur.execute("SELECT * FROM items")
+for result in sql_cur:
+    print(result)
+sql_cur.close()
 '''
 
-
-
-
-
-## routes
+# routes
 # for quick return tests
+
+
 @app.route('/api/hello', methods=['GET'])
 def api_hello():
     data = {
@@ -35,28 +38,39 @@ def api_hello():
         "password": "Testkey"
     }
     resp = jsonify(username="Test",
-        password="Testkey")
+                   password="Testkey")
     resp.status_code = 200
     return resp
 
 # for getting shop item details to the web-client
+
+
 @app.route('/api/item/details', methods=['GET', 'POST'])
 def api_item_details():
-    data = sql_get()
-    return data
+    sql_cur.execute("SELECT * FROM items LIMIT 0, 1")
+    data = sql_cur.fetchone()
+    resp = jsonify(id=data[0],
+                   name=data[1],
+                   description=data[2],
+                   numStock=data[3],
+                   price=data[4],
+                   picture=data[5]
+                   )
+    resp.status_code = 200
+    return resp
+
+
 @app.route('/api/item/list', methods=['GET', 'POST'])
 def api_item():
     data = sql_get()
     return data
 
 # for sending and applying score reviews from users
+
+
 @app.route('/api/item/score', methods=['GET', 'POST'])
 def api_item_score():
     data = request
-
-
-
-
 
 
 if __name__ == '__main__':
