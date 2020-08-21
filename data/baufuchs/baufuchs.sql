43542c82d41ba42bda0f2b868d77c2bdc544175e
Fabiattack <fabiattack@gmail.com>
2020-08-21 12:24:43 +0200

    added rating to item table

--- data/baufuchs/baufuchs.sql
+++ data/baufuchs/baufuchs.sql
@@ -1,50 +1,43 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server Version:               5.6.20 - MySQL Community Server (GPL)
--- Server Betriebssystem:        Win32
--- HeidiSQL Version:             9.3.0.4984
+-- Server Version:               10.4.14-MariaDB - mariadb.org binary distribution
+-- Server Betriebssystem:        Win64
+-- HeidiSQL Version:             11.0.0.5919
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Exportiere Struktur von Tabelle baufuchsos.category
+-- Exportiere Struktur von Tabelle baufuchs.category
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Exportiere Daten aus Tabelle baufuchsos.category: ~0 rows (ungefähr)
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
 
-
--- Exportiere Struktur von Tabelle baufuchsos.items
+-- Exportiere Struktur von Tabelle baufuchs.items
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `description` varchar(50) DEFAULT NULL,
-  `valueStock` int(11) NOT NULL DEFAULT '0',
-  `price` double NOT NULL DEFAULT '0',
+  `valueStock` int(11) NOT NULL DEFAULT 0,
+  `price` double NOT NULL DEFAULT 0,
   `picture` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Exportiere Daten aus Tabelle baufuchsos.items: ~0 rows (ungefähr)
-/*!40000 ALTER TABLE `items` DISABLE KEYS */;
-REPLACE INTO `items` (`id`, `name`, `description`, `valueStock`, `price`, `picture`) VALUES
-	(1, 'test', 'test', 1, 10, 'test');
-/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
 
-
--- Exportiere Struktur von Tabelle baufuchsos.ittoct
+-- Exportiere Struktur von Tabelle baufuchs.ittoct
 CREATE TABLE IF NOT EXISTS `ittoct` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) NOT NULL DEFAULT '0',
-  `item_id` int(11) NOT NULL DEFAULT '0',
+  `category_id` int(11) NOT NULL DEFAULT 0,
+  `item_id` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `FK_ittoct_category` (`category_id`),
   KEY `FK_ittoct_items` (`item_id`),
@@ -52,21 +45,31 @@ CREATE TABLE IF NOT EXISTS `ittoct` (
   CONSTRAINT `FK_ittoct_items` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Exportiere Daten aus Tabelle baufuchsos.ittoct: ~0 rows (ungefähr)
-/*!40000 ALTER TABLE `ittoct` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ittoct` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
 
-
--- Exportiere Struktur von Tabelle baufuchsos.ratings
+-- Exportiere Struktur von Tabelle baufuchs.ratings
 CREATE TABLE IF NOT EXISTS `ratings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `rating_value` int(11) NOT NULL DEFAULT '0',
+  `rating_value` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Exportiere Daten aus Tabelle baufuchsos.ratings: ~0 rows (ungefähr)
-/*!40000 ALTER TABLE `ratings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ratings` ENABLE KEYS */;
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Exportiere Struktur von Tabelle baufuchs.ratoit
+CREATE TABLE IF NOT EXISTS `ratoit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rating_id` int(11) NOT NULL DEFAULT 0,
+  `item_id` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `FK_ra_ratings` (`rating_id`),
+  KEY `FK_ra_items` (`item_id`),
+  CONSTRAINT `FK_ra_items` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
+  CONSTRAINT `FK_ra_ratings` FOREIGN KEY (`rating_id`) REFERENCES `ratings` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
