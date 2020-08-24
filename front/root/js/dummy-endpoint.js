740b4face1b0f6f48886939795a63207699ad7dd
IfSince <leonlaade@gmx.de>
2020-08-24 12:36:47 +0200

    dummy for HttpRequest

--- front/root/js/dummy-endpoint.js
+++ front/root/js/dummy-endpoint.js
@@ -5,6 +5,9 @@ xhr.onload = function (e) {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
       console.log(xhr.responseText);
+
+      response = xhr.responseText;
+
     } else {
       console.error(xhr.statusText);
     }
@@ -14,4 +17,6 @@ xhr.onerror = function (e) {
   console.error(xhr.statusText);
 };
 xhr.send(null);
+
+return response;
 }
