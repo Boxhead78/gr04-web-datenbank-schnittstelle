a220f4d7f55779b04d7b740fc7f58821f5962cc6
Lena Renneberg <lenarenneberg@web.de>
2020-08-31 14:41:41 +0200

    StartPageContent

---
+++ front/src/app/js/directives/StartPageContent/test.js
@@ -0,0 +1,8 @@
+app.directive('test', function() {
+    return {
+        restrict: 'E',
+        scope: {
+        },
+        templateUrl: 'js/directives/StartPageContent/StartPageContent.html'
+    }
+})
