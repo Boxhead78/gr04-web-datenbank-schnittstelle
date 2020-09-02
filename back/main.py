91f8227ab02987787941f492038a66375406861e
Jonas <Jojoleupolt@googlemail.com>
2020-09-02 20:46:49 +0200

    modified sql_con, worked on api_item()

--- back/main.py
+++ back/main.py
@@ -5,8 +5,6 @@ Modules: Flask, mysql-connector-python
 '''
 
 
-
-
 # importing modules
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -18,7 +16,7 @@ import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
-cors = CORS(app, resources= {
+cors = CORS(app, resources={
     r"/*": {
         "origins": "*"
     }
@@ -32,33 +30,24 @@ sql_db = "baufuchs"
 sql_buffered = True
 
 
-
-
-'''
 sql_con = mysql.connector.connect(
     host="127.0.0.1",
-    port="1337",
+    port="3306",
     user="root",
-    database="baufuchsos"
+    database="baufuchs"
 )
 sql_cur = sql_con.cursor(buffered=True)
 
-
+'''
 sql_cur.execute("SELECT * FROM items")
 data = sql_cur.fetchone()
 for result in data:
     print(result)
-<<<<<<< HEAD
-sql_cur.close()
-=======
-#sql_cur.close()
->>>>>>> caebba595a1576f4e0d7bce72bd561d5ccd88325
+# sql_cur.close()
 '''
 
 # routes
 # for quick return tests
-
-
 
 
 # routes
@@ -75,9 +64,11 @@ def api_hello():
     return resp
 
 # for getting shop item details to the web-client
+
+
 @app.route('/api/item/details', methods=['GET', 'POST'])
 def api_item_details():
-    sql_cur.execute("SELECT * FROM items LIMIT 0, 1")
+    sql_cur.execute("SELECT * FROM item LIMIT 0, 1")
     data = sql_cur.fetchone()
     data_item = {
         "id": data[0],
@@ -87,7 +78,8 @@ def api_item_details():
         "price": data[4],
         "picture": data[5]
     }
-    sql_cur.execute("SELECT * FROM items LIMIT 0, 1") # TODO: make this get scores of the item
+    # TODO: make this get scores of the item
+    sql_cur.execute("SELECT * FROM item LIMIT 0, 1")
     data = sql_cur.fetchone()
     data_scores = {
         "id": data[0]
@@ -101,31 +93,54 @@ def api_item_details():
 
 @app.route('/api/item/list', methods=['GET', 'POST'])
 def api_item():
-    #get filter information
+    # get filter information
     req = request.get_json()
-    #build sql query using WHERE to match above infos
-    sql_cur.execute(str("""SELECT * FROM items LIMIT """ + req.get("amount") + """
-        WHERE name CONTAINS """ + req.get("term") + """
-        WHERE price BETWEEN """ + req.get("costMin") + """ AND """ + req.get("costMax") + """
-        WHERE category EQUALS """ + req.get("cat"))
+    # build sql query using WHERE to match above infos
+    sql_cur.execute(("""SELECT * FROM item LIMIT '%s'
+        WHERE name CONTAINS '%s'
+        WHERE price BETWEEN %s AND %s
+        WHERE category EQUALS '%s' """ % (req.get("amount"), req.get("term"), req.get("costMin"), req.get("costMax"), req.get("cat"))
         )
-    #fetch all returned items
-    data = sql_cur.fetchall()
-    #loop over each item and build a json object with the general properties
+
+    # fetch all returned items
+    data=sql_cur.fetchall()
+    # loop over each item and build a json object with the general properties
+    item_list=[]
     for i in data:
-        print(i)
-    #return the json object
+        data_item={
+            "id": data[0],
+            "name": data[1],
+            "description": data[2],
+            "numStock": data[3],
+            "price": data[4],
+            "picture": data[5],
+            "color": data[6],
+            "creation_date": data[7],
+            "currency_id": data[8],
+            "weight": data[9],
+            "count": data[10],
+            "material": data[11],
+            "manufactorer_id": data[12],
+            "technical_details": data[13]
+        }
+        item_list.append(data_item)
+    # return the json object
+    resp=jsonify(item=item_list,
+                   score=item_list
+                   )
+    resp.status_code=200
+    return resp
 
 # for recieving and applying score reviews from users
-@app.route('/api/item/score', methods=['POST'])
+@ app.route('/api/item/score', methods=['POST'])
 def api_item_score():
-    #get score value etc from request
-    req = request.get_json()
-    #frontend needs to check for user registration
-    #add score to score table
+    # get score value etc from request
+    req=request.get_json()
+    # frontend needs to check for user registration
+    # add score to score table
     sql_cur.execute("""INSERT irgendwie * FROM items LIMIT 0, 1
     WHERE """ + req.get("val"))
-    #update score data in item table
+    # update score data in item table
 
 
 
