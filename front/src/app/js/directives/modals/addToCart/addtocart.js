65dce1f110b56e6d8803ac88f90db03109fc7515
Lena Renneberg <lenarenneberg@web.de>
2020-09-04 10:43:34 +0200

    pink stuff

---
+++ front/src/app/js/directives/modals/addToCart/addtocart.js
@@ -0,0 +1,8 @@
+app.directive('addtocart', function() {
+    return {
+        restrict: 'E',
+        scope: {
+        },
+        templateUrl: 'js/directives/modals/addToCart/addtocart.html'
+    }
+})
