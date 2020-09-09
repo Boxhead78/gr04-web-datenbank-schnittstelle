9c0fcaabbd21455bfa920d767b24fa182eaa2407
Lena Renneberg <lenarenneberg@web.de>
2020-09-09 13:03:44 +0200

    starRating

--- front/src/app/js/directives/content-Item/contentitem.js
+++ front/src/app/js/directives/content-Item/contentitem.js
@@ -1,6 +1,10 @@
 app.directive('contentitem', function() {
     return {
+        
         restrict: 'E',
         templateUrl: 'js/directives/content-Item/contentitem.html'
     }
 })
+
+var element = document.getElementById("star_1");
+element.classList.add("checked");
--- front/src/app/js/directives/modals/login/login.js
+++ front/src/app/js/directives/modals/login/login.js
@@ -6,3 +6,13 @@ app.directive('login', function() {
         templateUrl: 'js/directives/modals/login/login.html'
     }
 })
+
+ // Get the modal
+ var modal = document.getElementById('loginModal');
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
