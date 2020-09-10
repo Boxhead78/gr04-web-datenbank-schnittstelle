a7dcd53a966ab4f220754615cc89243c8a95bf8a
Lena Renneberg <lenarenneberg@web.de>
2020-09-10 14:05:21 +0200

    add addToCart modal to startpage and list

--- front/src/app/js/directives/content-Item/contentitem.js
+++ front/src/app/js/directives/content-Item/contentitem.js
@@ -2,9 +2,6 @@ app.directive('contentitem', function() {
     return {
         
         restrict: 'E',
-        templateUrl: 'js/directives/content-Item/contentitem.html'
+        templateUrl: 'js/directives/content-Item/contentitem.html',
     }
 })
-
-var element = document.getElementById("star_1");
-element.classList.add("checked");
